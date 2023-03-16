59bd68e619d110ff01c532c8c99caec652a6ac28
Stellamargy <stella.oloo@student.moringaschool.com>
2023-03-16 13:25:36 +0300

    passed tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    validate :not_to_allow_value
+    def not_to_allow_value
+        if title==="True Facts"
+            errors.add(:title, "Sorry no true facts for title")
+        end
+    end
 end
